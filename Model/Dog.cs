67910234d1b2e8b07073145758ebb218beca72c3
MariaDamyanova <mariagamianova@gmail.com>
2024-05-13 10:11:41 +0300

    .

---
+++ Model/Dog.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DogHouse.Model
+{
+    public class Dog
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+        // relation M M:1
+        public int BreedId { get; set; } //FK
+        public Breed Breeds { get; set; }//table with connect
+    }
+}
---
+++ Model/DogsDbContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DogHouse.Model
+{
+    public class DogsDbContext:DbContext
+    {
+        public DogsDbContext():base("DogsDbContext")
+        {
+
+        }
+        public DbSet<Dog> Dogs { get; set; }//creat table Dogs
+        public DbSet<Breed> Breeds { get; set; }//creat table Breeds
+    }
+}
